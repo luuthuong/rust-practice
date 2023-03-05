49cd8abd00478413eb90952117a0f93ac6a661cc
luuthuong <luuthuong200022000@gmail.com>
2023-03-05 18:11:45 +0700

    two_sum

---
+++ two_sum/src/two_sum.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct TwoSum {
+    pub numbs: Vec<i32>,
+    pub target: i32,
+}
+
+pub trait TwoSumTrait {
+    fn calc(&self) -> Option<[usize; 2]>;
+    fn print(&self);
+    fn new(numbs:&[i32], target: i32) -> TwoSum;
+}
+
+impl TwoSumTrait for TwoSum {
+    fn calc(&self) -> Option<[usize; 2]> {
+        let mut map = HashMap::new();
+        for (i, &num) in self.numbs.iter().enumerate() {
+            let complement = self.target - num;
+            if let Some(&j) = map.get(&complement) {
+                return Some([j, i]);
+            }
+            map.insert(num, i);
+        }
+        None
+    }
+
+    fn print(&self) {
+        println!("{:?} - target: {}", self.numbs, self.target);
+    }
+
+    fn new( numbs: &[i32], target: i32) -> TwoSum {
+        TwoSum{
+            target: target,
+            numbs: numbs.to_vec()
+        }
+    }
+}
